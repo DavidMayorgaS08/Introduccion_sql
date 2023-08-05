b844c8a2c4db22977ff1d921b31edb9853de7260
DavidMayorgaS08 <david08102@hotmail.com>
2023-08-04 22:22:18 -0500

    Correcciones ejer1

--- ejer1.sql
+++ ejer1.sql
@@ -1,6 +1,7 @@
 /*Título: Diseño de una Base de Datos para una Tienda en Línea
 
 Descripción: Imagina que estás trabajando como consultor de bases de datos para una tienda en línea que vende una variedad de productos. Tu tarea es diseñar la estructura de la base de datos utilizando MySQL y el lenguaje DDL. Debes definir las tablas necesarias para almacenar información sobre los productos, los clientes y los pedidos. Asegúrate de considerar las relaciones entre estas entidades y aplicar buenas prácticas de diseño de bases de datos.
+la relacion del pedido y los productos debe ser atravez de una factura
 */
 
 create database tienda;
@@ -8,26 +9,36 @@ create database tienda;
 use tienda;
 
 create table productos(
-    id int primary key,
-    nombre varchar(50),
-    precio int,
-    cantidad int
+    id int not null,
+    nombre varchar(50) not null,
+    precio float not null,
+    descripcion varchar(100) not null,
+    primary key(id)
 );
 
 create table clientes(
-    id int primary key,
-    nombre varchar(50),
-    apellido varchar(50),
-    email varchar(50),
-    telefono int
+    id int not null,
+    nombre varchar(50) not null,
+    apellido varchar(50) not null,
+    email varchar(50) not null,
+    primary key(id)
 );
 
 create table pedidos(
-    id int primary key,
-    id_cliente int,
-    id_producto int,
-    fecha date,
-    foreign key(id_cliente) references clientes(id),
+    id int not null,
+    fecha date not null,
+    cliente_id int not null,
+    primary key(id),
+    foreign key(cliente_id) references clientes(id)
 );
 
-ALTER TABLE pedidos ADD FOREIGN KEY (id_producto) references productos(id);
+create table factura(
+    id int not null,
+    pedido_id int not null,
+    producto_id int not null,
+    cantidad int not null,
+    descripcion varchar(100) not null,
+    primary key(id),
+    foreign key(pedido_id) references pedidos(id),
+    foreign key(producto_id) references productos(id)
+);
